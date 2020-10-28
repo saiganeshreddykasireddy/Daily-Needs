9e4b9a967b96394f8d126046999e293d11765c0f
ganeshreddy <reddyganesh11192@gmail.com>
2020-10-28 21:49:05 +0530

    changes

--- src/AppTopbar.js
+++ src/AppTopbar.js
@@ -19,15 +19,8 @@ export class AppTopbar extends Component {
                     <span className="pi pi-bars"/>
                 </button>
                 <div className="layout-topbar-icons">
-                    <span className="layout-topbar-search">
-                        <InputText type="text" placeholder="Search" />
-                        <span className="layout-topbar-search-icon pi pi-search"/>
-                    </span>
-                    <button className="p-link">
-                        <span className="layout-topbar-item-text">Events</span>
-                        <span className="layout-topbar-icon pi pi-calendar"/>
-                        {/* <span className="layout-topbar-badge">5</span> */}
-                    </button>
+                    
+                    
                     <button className="p-link">
                         <span className="layout-topbar-item-text">Settings</span>
                         <span className="layout-topbar-icon pi pi-cog"/>
