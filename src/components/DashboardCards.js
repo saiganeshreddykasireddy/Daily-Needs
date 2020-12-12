748761f66d94095efa50c77ab2707083d1e0c882
ganeshreddy <reddyganesh11192@gmail.com>
2020-12-12 22:20:23 +0530

    quantity error fix

--- src/components/DashboardCards.js
+++ src/components/DashboardCards.js
@@ -9,19 +9,26 @@ import "./Dashboard.scss";
     let itemTypes = Object.keys(allRequests) || {};
     return itemTypes.map((key) => {
         let RequestsOfSameType = Object.values(allRequests[key]) || [];
-        const data = _.groupBy(RequestsOfSameType, "Brand");
-        const pieData = [];
-        Object.keys(data).forEach(function (key) {
-            let Data = [key, data[key].length] || [];
-            pieData.push(Data);
+        const data = _.groupBy(RequestsOfSameType, "Brand")||{};
+        console.log(data,"data");
+        const cardsData = [];
+        Object.keys(data).forEach(function (_key,index) {
+            let count = data[_key];
+            let _count =0;
+            console.log(count,"count");
+            count.map((_Item)=>{
+                _count = _count+_Item.Quantity;
+            })
+            let Data = [_key, _count] || [];
+            cardsData.push(Data);
         });
-        pieData.sort();
+        cardsData.sort();
         return (
             <React.Fragment>
                 <div className="CardsList">
                 <div className="Cardheader">{key}</div>
                 <div className="Cardcontent">
-                    {pieData&& pieData.length ? pieData.map((_item) => {
+                    {cardsData&& cardsData.length ? cardsData.map((_item) => {
                         return <div className="List"><span className="brandname">{_item[0]}</span><span className="brandcount">{_item[1]}</span></div>
                     }) : <p>No Orders Found</p>}
                 </div>
